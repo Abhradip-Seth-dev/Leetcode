71caa4ebc501f7b73e7b928326b5763d2c2df5ce
Abhradip Seth <abhradipsethofficial06@gmail.com>
2026-04-24 10:52:24 +0530

    updated

---
+++ LC-349/main.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set<int> st(nums1.begin(),nums1.end());
+        set<int> result;
+        for(int num : nums2){
+            if(st.count(num)){
+                result.insert(num);
+            }
+        }
+
+        
+        
+        return vector<int>(result.begin(),result.end());
+    }
+};
