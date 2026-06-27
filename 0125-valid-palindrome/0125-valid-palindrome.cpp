a52255e6578f2e933b188332a053f98d3fe7eddb
Abhradip Seth <abhradipsethofficial06@gmail.com>
2026-06-27 23:50:04 +0530

    Time: 2 ms (36.31%), Space: 10.3 MB (24.97%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n=s.size();
+        string news="";
+        for(int i=0;i<n;i++){
+            if(!isalnum(s[i]) || s[i]==' '){
+                continue;
+            }
+            news+=tolower(s[i]);
+        
+        }
+        int newsl=news.size();
+        int left = 0;
+        int right = newsl - 1;
+        for(int i=0;i<newsl/2;i++){
+            if(news[left]==news[right]){
+                left++;
+                right--;
+            }
+            else return false;
+        }   
+        return true;
+    }
+};
