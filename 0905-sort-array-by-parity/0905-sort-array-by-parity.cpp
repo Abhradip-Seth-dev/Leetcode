789e803049fef461dceb7a8fbbbeacba7e46202c
Abhradip Seth <abhradipsethofficial06@gmail.com>
2026-07-04 16:12:18 +0530

    Time: 0 ms (100%), Space: 20.8 MB (23.75%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+// actually we can solve this problem with dutch national flag algortihm but we use this , we also can use that both has 0(n) time complexity
     vector<int> sortArrayByParity(vector<int>& nums) {
         vector<int> res;
         int n=nums.size();
