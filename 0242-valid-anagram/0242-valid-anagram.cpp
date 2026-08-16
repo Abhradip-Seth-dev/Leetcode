71e181f7ca04c2804d904cc96bcc0b89d0419820
Abhradip Seth <abhradipsethofficial06@gmail.com>
2026-08-17 01:05:21 +0530

    Time: 0 ms (100%), Space: 10 MB (11.55%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,11 +2,20 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        for(int i=0;i<s.size();i++){
-            if(s[i]!=t[i]) return false;
+        unordered_map<char,int>f1;
+        unordered_map<char,int>f2;
+        for(char c : s){
+            f1[c]++;
+        }
+        for(char c : t){
+            f2[c]++;
+        }
+        for(char c :s){
+            if(f1[c]!=f2[c]) return false;
         }
         return true;
+
+
+
     }
 };
