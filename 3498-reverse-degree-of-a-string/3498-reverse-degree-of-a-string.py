53893d1b090ee482139bcf56f67a92600d4bc608
Abhradip Seth <abhradipsethofficial06@gmail.com>
2026-09-20 23:28:57 +0530

    Time: 0 ms (100%), Space: 19.2 MB (55.57%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+        freq = defaultdict(int)
+        product ,suum= 1,0
+        charlist='abcdefghijklmnopqrstuvwxyz'
+        num=27
+        for char in charlist:
+            num-=1
+            freq[char]=num
+        for i in range(0,len(s)):
+            suum+=product*((i+1)*freq[s[i]])
+
+        return suum       
+            
+            
+        
